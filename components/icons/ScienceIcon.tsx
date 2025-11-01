ed29ed6318ee72d4987cda9f231f65b3a02645f0
HamsAssem <142051365+HamsAssem@users.noreply.github.com>
2025-11-01 23:39:41 +0200

    Delete components directory

--- components/icons/ScienceIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const ScienceIcon: React.FC = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19.428 15.428a2 2 0 00-1.022-.547l-2.363-.448c-.57-.108-1.14-.108-1.71 0l-2.363.448a2 2 0 00-1.022.547m-3.238 0a2 2 0 00-1.022-.547l-2.363-.448c-.57-.108-1.14-.108-1.71 0l-2.363.448a2 2 0 00-1.022.547M12 18.5v-15m-3.379 6.621a2 2 0 00-2.828 0M12 18.5a2 2 0 002 2h2a2 2 0 002-2M12 18.5a2 2 0 012 2h2a2 2 0 012-2m-6 3.5c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z" />
-    </svg>
-);
